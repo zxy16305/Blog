a9be9d2ae49c312b0993008b632690cbab9151dd
Kiswich <zxy16305@gmail.com>
2018-10-17 22:26:07 +0800

    调整了git pull的延迟

--- BlogSynchronize.py
+++ BlogSynchronize.py
@@ -9,12 +9,14 @@ PORT = 15622
 Protocol = "HTTP/1.1"
 git_origins = "https://github.com/zxy16305/Blog"
 hexo_current_working_directory = "/root/applications/hexo/"
+delay_git = 30
+delay_hexo = 30
 
 
 def git_pull():
     os.system("git pull " + git_origins)
     # print("git pull")
-    t = threading.Timer(20, hexo_generate)
+    t = threading.Timer(delay_hexo, hexo_generate)
     t.start()
 
 
@@ -30,7 +32,7 @@ class myHandler(HTTP.BaseHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(bytes("OK", encoding="utf-8"))
-            t = threading.Timer(1, git_pull)
+            t = threading.Timer(delay_git, git_pull)
             t.start()
         return
 
