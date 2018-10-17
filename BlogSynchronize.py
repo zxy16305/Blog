b887afb9b10fc303a8d4d0d69481396148ddc45e
Kiswich <zxy16305@gmail.com>
2018-10-17 21:53:56 +0800

    linux

--- BlogSynchronize.py
+++ BlogSynchronize.py
@@ -2,11 +2,14 @@ import http.server as HTTP
 import socketserver
 import os
 import time
+import threading
+import logging
 
 PORT = 15622
 Protocol = "HTTP/1.1"
 git_origins = "https://github.com/zxy16305/Blog"
 hexo_current_working_directory = "/root/applications/hexo/"
+
 
 class myHandler(HTTP.BaseHTTPRequestHandler):
     def do_POST(self):
@@ -16,11 +19,15 @@ class myHandler(HTTP.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes("OK", encoding="utf-8"))
             os.system("git pull " + git_origins)
-            time.sleep(10)
-            os.system("hexo --cwd " + hexo_current_working_directory + " generate")
+            t = threading.Timer(10, hexo_generate, args=())
+            t.start()
         return
 
 
 with socketserver.TCPServer(("", PORT), myHandler) as httpd:
     print("com in")
     httpd.serve_forever()
+
+
+def hexo_generate():
+    os.system("hexo --cwd " + hexo_current_working_directory + " generate")
